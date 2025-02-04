829ea33aa706c6ba5fc1f8829d85b87fa0f1bdd6
yee <yeelysia@qq.com>
2025-02-05 02:22:52 +0800

    mobile improve #2

--- themes/medium/index.js
+++ themes/medium/index.js
@@ -150,7 +150,7 @@ const LayoutBase = props => {
                   <div className='w-full flex flex-row justify-center'>
                     <div
                       id='container-inner'
-                      className={`px-7 ${fullWidth || router.pathname === '/' ? '' : 'max-w-5xl'} justify-center mx-auto min-h-screen`}>
+                      className={`px-7 ${fullWidth || router.pathname === '/' ? '' : 'max-w-5xl'} justify-center mx-8 min-h-screen`}>
                       <Transition
                         show={!onLoading}
                         appear={true}
@@ -169,7 +169,7 @@ const LayoutBase = props => {
                     </div>
 
                     {/* 右侧栏 */}
-                    <SideRight {...props}/>
+                    <SideRight {...props} />
                   </div>
                 </div>
               )}
